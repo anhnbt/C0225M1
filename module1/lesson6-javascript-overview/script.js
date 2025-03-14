f144e0a9286556bc783a21de55bd0dc27b2bbfcc
Nguyễn Bá Tuấn Anh <anh.nguyen16@codegym.vn>
2025-03-14 23:41:40 +0700

    Add lesson6: Javascript overview

---
+++ module1/lesson6-javascript-overview/script.js
@@ -0,0 +1,5 @@
+// Hiển thị hộp thoại chào mừng
+alert("Hello World! Đây là JavaScript được nhúng từ file bên ngoài.");
+
+// Ghi log vào console
+console.log("File script.js đã được tải thành công!");
